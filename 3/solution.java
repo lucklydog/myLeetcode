2d346bb22c63ef2e1b4746f75eca00777ccc92f2
lucklydog <32816666+lucklydog@users.noreply.github.com>
2018-01-23 15:25:54 +0800

    第三题的两种解法

--- 3/solution.java
+++ 3/solution.java
@@ -32,3 +32,29 @@ calss solution {
 #####
 第二种算法，这种算法的时间复杂度为O(n)
 思路是：
+给出一个字符串例如：dacbaedc
+利用一个HashSet存储未重复的子字符串
+如果在HashSet中为出现的字符，就存储进去，并且将所给的字符串的下标往后移动一位
+动作：
+d为出现，add，j++ ->  a未出现，add，j++  -> c 未出现，add  -> b 未出现，add -> a 出现，那么找到上一次出现a的位置，i++
+在这过程中设置一个变量，记录最长的子字符串。
+
+代码：
+class solution  {
+  public int lengthOfLongSubstring(String s){
+      int  n = s.length();//字符串的长度
+      int len  = 0;//记录子字符串的长度
+      Set <character> set = new HashSet();//定义哈斯列表
+      int  i = 0,j = 0;//标记下标
+      while(i<n && j<n){
+        if(!set.contains(s.charAt(j))){
+          set.add(s.charAt(j++));
+          len = Math.max(len,j-i);//记录最长子字符串
+        }
+        else {
+          set.remove(i++);//前标记往后移动 直到找到相同字符下标
+        }
+      }
+      return len;//返回最长的子字符串的长度
+  }
+}
